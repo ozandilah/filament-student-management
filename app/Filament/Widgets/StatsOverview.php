bd6a1dcc682201c8d586bdb5b553bcb9ff033bea
ozandilah <ozan.it212@gmail.com>
2024-09-05 21:03:05 +0700

    Finish Custom Dashboard using filament panel builder adding widget overview and widget table data

---
+++ app/Filament/Widgets/StatsOverview.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Classes;
+use App\Models\Section;
+use App\Models\Student;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+
+class StatsOverview extends BaseWidget
+{
+    protected static ?int $sort = 1;
+    protected function getStats(): array
+    {
+        return [
+            Stat::make('Total Classes',Classes::count()),
+            Stat::make('Total Sections', Section::count()),
+            Stat::make('Total Students', Student::count()),
+        ];
+    }
+}
